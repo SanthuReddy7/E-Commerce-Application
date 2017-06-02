888267b7900ba6c57fff6dfac55ba474e329c84e
unknown <santhoshreddy.basi@gmail.com>
2017-06-02 16:27:18 -0400

    logout updated in admin.jsp

--- eMusicStore/src/main/java/com/emusicstore/dao/ProductDao.java
+++ eMusicStore/src/main/java/com/emusicstore/dao/ProductDao.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 public interface ProductDao {
     void addProduct(Product product);
-void editProduct(Product product);
+    void editProduct(Product product);
     Product getProductById(String id);
 
     List<Product> getAllProducts();
